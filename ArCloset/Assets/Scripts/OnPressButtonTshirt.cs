b31019a6a41914cbdda8163bd889ae35136a6be5
Berni <berni.cortvriend@student.howest.be>
2020-12-02 13:50:52 +0100

    push kleren in kast

---
+++ ArCloset/Assets/Scripts/OnPressButtonTshirt.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnPressButtonTshirt : MonoBehaviour
+{
+    private Button button;
+    private ColorBlock theColor;
+
+    void Start()
+    {
+        //print("hallo");
+        SaveComponents();
+    }
+
+    private void SaveComponents()
+    {
+        button = gameObject.GetComponent<Button>();
+        theColor = gameObject.GetComponent<Button>().colors;
+    }
+
+
+    public void OnMouseDown()
+    {
+        Debug.Log("Button was pressed!");
+        changeColorButton();
+
+
+    }
+
+    private void changeColorButton()
+    {
+        Debug.Log("ChangedColor");
+        theColor.highlightedColor = Color.blue;
+        //theColor.pressedColor = Color.green;
+    }
+}
